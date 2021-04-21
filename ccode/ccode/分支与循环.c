e1bbfac9ef35a16e813f9f67035a29ba709ba799
ty68 <13718915562@139.com>
2021-04-21 21:41:46 +0800

    if分支

---
+++ ccode/ccode/分支与循环.c
@@ -0,0 +1,22 @@
+﻿//#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int age = 0;
+//	scanf("%d", &age);
+//	if(age<18)
+//		printf("未成年\n");
+//	else
+//	{
+//		if(age>=18 && age<28)
+//			printf("青年\n");
+//		else if(age>=28 && age<50)
+//			printf("壮年\n");
+//		else if(age>=50 && age<90)
+//			printf("老年\n");
+//		else
+//			printf("老不死\n");
+//	}
+//}
